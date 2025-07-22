5c66e031c6967f3045ef78db57e7d8cc0bbd2eb8
yeaminrabbi <yeaminrabbi308@gmail.com>
2025-07-22 13:20:04 +0600

    project file cleanup

--- resources/views/filament/resources/why-choose-us-reseource-resource/pages/custom-edit.blade.php
+++
@@ -1,9 +0,0 @@
-<x-filament-panels::page>
-    <x-filament-panels::form wire:submit.prevent="submitForm">
-        {{ $this->form }}
-
-        <x-filament::button type="submit" class="mt-4">
-            Save Changes
-        </x-filament::button>
-    </x-filament-panels::form>
-</x-filament-panels::page>
